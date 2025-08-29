9d2b72864ef496a7ba93ef67179f7191e66015c3
Wataru TOYA <watrty@gmail.com>
2025-08-30 08:13:25 +0900

    otel

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require 'sinatra'
+require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/otlp'
+require 'opentelemetry/instrumentation/sinatra'
+
+OpenTelemetry::SDK.configure do |c|
+  c.service_name = 'tail-sampling-test'
+  c.use 'OpenTelemetry::Instrumentation::Sinatra'
+end
 
 get '/' do
   'Hello, world!'
